7a4c81515dcbb57beeceb93f1ccc9100bc9accf2
Ania <aniaa.pawlik@gmail.com>
2025-05-22 23:13:16 +0200

    Add loading/failed states + refactor useEffect and formattedNews

--- my-app/src/components/NewsTicker/NewsTicker.tsx
+++ my-app/src/components/NewsTicker/NewsTicker.tsx
@@ -1,24 +1,30 @@
 import React, { FC, useEffect, useMemo } from "react";
-import { StyledWrapper } from "./NewsTicker.styles.js";
+import {
+  StyledWrapper,
+  StyledLoader,
+  LoaderWrapper,
+  ContentWrapper,
+} from "./NewsTicker.styles.js";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNews } from "../../store/news.ts";
 import { RootState, AppDispatch } from "../../store/store";
 
 export const NewsTicker: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const news = useSelector((state: RootState) => state.news.items);
+  const { items: news, status } = useSelector((state: RootState) => state.news);
 
   useEffect(() => {
     dispatch(fetchNews());
-  }, [dispatch]);
+    // eslint-disable-next-line
+  }, []);
 
   const formattedNews = useMemo(() => {
     return news.map((item) => {
       const parsedDate = new Date(item.datetime * 1000);
-      const getFullYear = parsedDate.getFullYear();
-      const getMonth = String(parsedDate.getMonth() + 1).padStart(2, "0");
-      const getDay = String(parsedDate.getDate()).padStart(2, "0");
-      const formattedDate = `${getDay}/${getMonth}/${getFullYear}`;
+      const fullYear = parsedDate.getFullYear();
+      const month = String(parsedDate.getMonth() + 1).padStart(2, "0");
+      const day = String(parsedDate.getDate()).padStart(2, "0");
+      const formattedDate = `${day}/${month}/${fullYear}`;
 
       return {
         ...item,
@@ -27,14 +33,32 @@ export const NewsTicker: FC = () => {
     });
   }, [news]);
 
+  const handleRefresh = () => {
+    dispatch(fetchNews());
+  };
+
   return (
     <StyledWrapper>
-      {formattedNews.map((item) => (
-        <div key={`${item.datetime}-${item.headline}`}>
-          <p>{item.formattedDate}: </p>
-          <p>{item.headline}</p>
-        </div>
-      ))}
+      {status === "loading" && (
+        <LoaderWrapper>
+          <StyledLoader />
+        </LoaderWrapper>
+      )}
+
+      {status === "failed" && (
+        <ContentWrapper>
+          <p>Failed to load news.</p>
+          <button onClick={handleRefresh}>Try Again</button>
+        </ContentWrapper>
+      )}
+
+      {status === "idle" &&
+        formattedNews.map((item) => (
+          <ContentWrapper key={`${item.datetime}-${item.headline}`}>
+            <p>{item.formattedDate}: </p>
+            <p>{item.headline}</p>
+          </ContentWrapper>
+        ))}
     </StyledWrapper>
   );
 };
